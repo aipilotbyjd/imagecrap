5a12b32fdfadcac6a1f5644895f55c76af045c82
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-04-03 18:46:53 +0800

    sS

---
+++ components/mainapp/inputs/Promptinput.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import styles from "./promptinput.style";
+
+const Promptinput = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Prompt INput</Text>
+    </View>
+  );
+};
+export default Promptinput;
