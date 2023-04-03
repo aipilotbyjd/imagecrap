13f40e85f313d359f0831454bd1fe39637229fdb
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-04-03 18:37:35 +0800

    oqosq

---
+++ app/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+      <View>
+        <Text>Home</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
